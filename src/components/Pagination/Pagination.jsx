cb6a1392867def7f1f2547fc27a044907c737ff4
Ramil <ramik_96_08@mail.ru>
2024-08-20 20:38:14 +0600

    add pagination, change api endpoint

---
+++ src/components/Pagination/Pagination.jsx
@@ -0,0 +1,44 @@
+import styles from "./Pagination.module.css";
+
+function Pagination({
+  totalPages,
+  handleNextPage,
+  handlePageClick,
+  handlePreviousPage,
+  currentPage,
+}) {
+  return (
+    <div className={styles.pagination}>
+      <button
+        onClick={handlePreviousPage}
+        disabled={currentPage <= 1}
+        className={styles.arrow}
+      >
+        {"<"}
+      </button>
+      <div className={styles.list}>
+        {[...Array(totalPages)].map((_, index) => {
+          return (
+            <button
+              onClick={() => handlePageClick(index + 1)}
+              key={index}
+              className={styles.pageNumber}
+              disabled={index + 1 === currentPage}
+            >
+              {index + 1}
+            </button>
+          );
+        })}
+      </div>
+      <button
+        onClick={handleNextPage}
+        disabled={currentPage >= 10}
+        className={styles.arrow}
+      >
+        {">"}
+      </button>
+    </div>
+  );
+}
+
+export default Pagination;
